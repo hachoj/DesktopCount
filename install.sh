ef00370c93d99036d3afaa9e4170dd122c339215
Harrison Chojnowski <92695466+hachoj@users.noreply.github.com>
2025-06-11 12:39:29 -0400

    Add ApplicationServices framework to build

--- install.sh
+++ install.sh
@@ -6,7 +6,10 @@ rm -rf "$APP.app"
 mkdir -p "$APP.app/Contents/MacOS"
 cp Info.plist "$APP.app/Contents/Info.plist"
 
-swiftc Sources/DesktopCount.swift -framework Cocoa -o "$APP.app/Contents/MacOS/$APP"
+swiftc Sources/DesktopCount.swift \
+    -framework Cocoa \
+    -framework ApplicationServices \
+    -o "$APP.app/Contents/MacOS/$APP"
 
 cp -R "$APP.app" "/Applications/$APP.app"
 
